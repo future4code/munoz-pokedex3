bf854e37f535729725fa9ec63244ffaa268ba804
Brendha Luiza Barbosa Pereira <barbosabrendhaluiza@gmail.com>
2021-07-07 20:51:27 -0300

    header

--- pokedex-labenu/src/pages/home/index.jsx
+++ pokedex-labenu/src/pages/home/index.jsx
@@ -1,5 +1,9 @@
 import { useHistory } from "react-router-dom";
 import React from "react";
+import { HeaderAppBar } from "./style";
+import { HeaderToolbar } from "./style";
+import { Container } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 
 export function Home() {
   const history = useHistory();
@@ -7,8 +11,15 @@ export function Home() {
   const detalhes = () => history.push("details");
   return (
     <div>
-      home!
-      <button onClick={irParaPokedex}>Pokedex</button>
+      <HeaderAppBar position={"sticky"}>
+        <HeaderToolbar component={Container}>
+          Pokemóns
+          <Button variant="outlined" onClick={irParaPokedex}>
+            Pokedex
+          </Button>
+        </HeaderToolbar>
+      </HeaderAppBar>
+
       <button onClick={detalhes}>Detalhes</button>
     </div>
   );
---
+++ pokedex-labenu/src/pages/home/style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { AppBar, Toolbar } from "@material-ui/core";
+// import { theme } from "../../themes/index";
+
+export const HeaderAppBar = styled(AppBar)`
+  background-color: ${(props) => props.theme.main};
+`;
+export const HeaderToolbar = styled(Toolbar)`
+  display: flex;
+  justify-content: space-between;
+  gap: 16px;
+  flex-wrap: wrap;
+`;
