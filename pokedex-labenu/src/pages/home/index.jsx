80ba19c7368bb2c875b2dbbc048f434f25523bce
Brendha Luiza Barbosa Pereira <barbosabrendhaluiza@gmail.com>
2021-07-07 10:40:40 -0300

    botões

--- pokedex-labenu/src/pages/home/index.jsx
+++ pokedex-labenu/src/pages/home/index.jsx
@@ -1,5 +1,17 @@
-function Home() {
-  return <div>home!</div>;
+import { useHistory } from "react-router-dom";
+import React from "react";
+
+export function Home() {
+  const history = useHistory();
+  const irParaPokedex = () => history.push("pokedex");
+  const detalhes = () => history.push("details");
+  return (
+    <div>
+      home!
+      <button onClick={irParaPokedex}>Pokedex</button>
+      <button onClick={detalhes}>Detalhes</button>
+    </div>
+  );
 }
 
 export default Home;
--- pokedex-labenu/src/pages/pokedex/index.jsx
+++ pokedex-labenu/src/pages/pokedex/index.jsx
@@ -1,5 +1,17 @@
-function Pokedex() {
-  return <div>Pokedex!</div>;
+import { useHistory } from "react-router-dom";
+import React from "react";
+
+export function Pokedex() {
+  const history = useHistory();
+  const voltar = () => history.push("home");
+  const detalhes = () => history.push("details");
+  return (
+    <div>
+      Pokedex!
+      <button onClick={voltar}>Voltar</button>
+      <button onClick={detalhes}>Detalhes</button>
+    </div>
+  );
 }
 
 export default Pokedex;
