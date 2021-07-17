0c47abf220c6b7678e4fe1c11573edc2e96c1dcf
Matheus <matheusrdeoliv1@gmail.com>
2021-07-17 10:54:52 -0300

    Implementei a parte de tras do card

--- pokedex-labenu/src/pages/home/index.jsx
+++ pokedex-labenu/src/pages/home/index.jsx
@@ -6,11 +6,12 @@ import {
   Card,
   CardsContainer,
   ContainerButton,
-  ContainerNomeBotão,
+  ContainerTitle,
   FlipContainer,
   FrontFlipper,
   PokemonContainer,
   PokemonImage,
+  BackFlipper
 } from "./style";
 
 import { Container } from "@material-ui/core";
@@ -29,7 +30,7 @@ export function Home(props) {
     if (flippedCardId === id) {
       return setFlippedCardId('')
     }
-      setFlippedCardId(id)
+    setFlippedCardId(id)
   }
 
   const addPokedex = (pokemon, index) => {
@@ -50,8 +51,8 @@ export function Home(props) {
   }).map((pokemon, index) => {
     return (
       <Card>
-        <FlipContainer 
-        className="flip-container" 
+        <FlipContainer
+          className="flip-container"
         >
           <PokemonContainer
             flippedCardId={flippedCardId}
@@ -68,17 +69,20 @@ export function Home(props) {
                 width="110px"
                 height="110px"
               />
-              <ContainerNomeBotão>
+              <ContainerTitle>
                 <h2>{pokemon.nome}</h2>
-                <p>{pokemon.tipo}</p>
-                <ButtonCard onClick={() => addPokedex(pokemon)}>
-                  {/* {adicionou ? "Remover" : "Adicionar"} */}
-                  Adicionar à Pokedex
-                </ButtonCard>
-              </ContainerNomeBotão>
+              </ContainerTitle>
             </FrontFlipper>
-            <div className="back">
-            </div>
+            <BackFlipper className="back">
+              <p>Pokémon nº: {pokemon.id}</p>
+              <p>Peso: {pokemon.peso}</p>
+              <p>Tamanho: {pokemon.tamanho}</p>
+              <p>Tipo: {pokemon.tipo}</p>
+              <ButtonCard onClick={() => addPokedex(pokemon)}>
+                {/* {adicionou ? "Remover" : "Adicionar"} */}
+                Adicionar à Pokedex
+              </ButtonCard>
+            </BackFlipper>
           </PokemonContainer>
         </FlipContainer>
       </Card>
